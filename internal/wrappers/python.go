df617027d5546f1b6ffb94294efcf8b6bdea375b
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-09-03 18:38:22 -0700

    Add python code submission wrapper

---
+++ internal/wrappers/python.go
@@ -0,0 +1,18 @@
+package wrapper
+
+const PythonWrapper = `
+import json
+from submission import user_solution
+
+with open("cases.json") as f:
+	cases = json.load(f)
+for case in cases:
+	raw = case["input"]
+
+	try:
+		converted = json.loads(raw)
+	except json.JSONDecodeError:
+		converted = raw
+	
+	print(user_solution(case))
+`
