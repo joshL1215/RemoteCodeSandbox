95559da3d014fb69912e499be7f7a02737234431
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-09-04 00:02:58 -0700

    Add additional isolation/security configs for container

--- internal/docker/spawner.go
+++ internal/docker/spawner.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -14,11 +15,13 @@ import (
 // Spawns a code execution container
 func SpawnJob(cli *client.Client, language string, code string, cases []models.Case) (string, error) {
 
-	ctx := context.Background()
+	// 30 sec timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
 	// Selects image for language
 	images := map[string]string{
-		"python": "python:3.12-alpine",
+		"python": "python:3.12-alpine", // TODO: probably add pre-pulled images to main.go
 		"node":   "node:20-alpine",
 	}
 
@@ -39,12 +42,23 @@ func SpawnJob(cli *client.Client, language string, code string, cases []models.C
 		Cmd:        []string{"python3", "/app/main.py"}, // TODO: other language support, this only allows Python
 		WorkingDir: "/app",
 		Tty:        false,
+		User:       "1000:1000",
 	}
 
 	hostConfig := &container.HostConfig{ // TODO: Add gVisor runtime for isolation
 		Binds: []string{
-			fmt.Sprintf("%s:/app", jobDir),
+			fmt.Sprintf("%s:/app:ro", jobDir),
 		},
 		AutoRemove: true,
+		Tmpfs: map[string]string{
+			"/tmp": "size=64m",
+		},
+		CapDrop: []string{"ALL"},
+		Resources: container.Resources{
+			Memory:   64 * 1024 * 1024, // give container 64mb memory max
+			NanoCPUs: 100_000_000,      // give container .1 cpus
+		},
+		NetworkMode:    "none",
+		ReadonlyRootfs: true,
 	}
 }
