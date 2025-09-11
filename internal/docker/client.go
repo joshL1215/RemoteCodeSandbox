9dbe1f6f002c83c6019cc1af48d8d738afdd8ab7
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-09-11 13:44:00 -0700

    Fix access to client's access to docker daemon

--- internal/docker/client.go
+++ internal/docker/client.go
@@ -7,7 +7,10 @@ import (
 )
 
 func ConnectToDaemon() (*client.Client, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
+	cli, err := client.NewClientWithOpts(
+		client.WithHost("unix:///var/run/docker.sock"),
+		client.WithVersion("1.51"),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to docker daemon: %w", err)
 	}
